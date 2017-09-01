87396c3ebbdd22d9393910efc40408d2f4696ebe
AtkLordOverAll <darkninjabuni@gmail.com>
2017-09-01 14:35:21 +0100

    added prune

--- selfbot.js
+++ selfbot.js
@@ -7,4 +7,26 @@ client.on("ready", () => {
     console.log("Bot loaded.\n");
 });
 
+client.on("message", message => {
+    // stops running if message isn't from user or doesn't have prefix
+    if (message.author !== client.user || !message.startsWith(config.prefix)){
+        return;
+    }
+
+    const args = message.content.split(" ").slice(1);
+
+    if (message.content.startsWith(config.prefix + "prune")) {
+        message.channel.fetchMessages({limit: 100})
+        .then(messages => {
+            let messageArray = messages.array();
+            messageArray = messageArray.filter(m => m.author.id === client.user.id);
+            messageArray.length = message.count + 1; // test this line
+            messageArray.map(m => m.delete().catch(console.error));
+        });
+    }
+
+    
+
+});
+
 client.login(config.token);
